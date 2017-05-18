e3dea33b8a16048d49467db52b276ea1e3a6b556
elton-faustino <elton-faustino@hotmail.com>
2017-05-18 20:11:08 -0300

    Add files via upload

--- NuApi/nu.requestWeak/User.cs
+++ NuApi/nu.requestWeak/User.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace nu.requestWeak
 {
+    public class RootClass
+    {
+        public User uSER { get; set; }
+    }
+
+
     public class User
     {
         public string Conta { get; set; }
 
         public string Senha { get; set; }
+
+        public List<string> Teste { get; set; }
     }
 }
